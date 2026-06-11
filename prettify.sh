65b8e6ea1d194596d9463eb14824a27046d9ff20
Iv Robinson <aerobinsoniv@gmail.com>
2026-06-10 22:40:50 -0700

    one blank line between functions

--- prettify.sh
+++ prettify.sh
@@ -2,6 +2,9 @@
 cargo clippy --fix --allow-dirty --allow-staged
 cargo fmt
 
+# hacky slop to enforce one blank line between functions
+gawk -i inplace 'BEGINFILE { prev = "" } prev == "}" && $0 != "" { print "" } { print; prev = $0 }' */src/*.rs
+
 ruff check --fix pylib
 ruff format pylib
 
