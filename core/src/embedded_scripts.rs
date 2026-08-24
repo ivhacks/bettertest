826741e3f824bb35816b39b38e92689be4f4b1c5
Iv Robinson <aerobinsoniv@gmail.com>
2026-08-23 20:13:06 -0700

    todo

--- core/src/embedded_scripts.rs
+++ core/src/embedded_scripts.rs
@@ -3,6 +3,8 @@ pub const GLUE_SCRIPT: &str = include_str!("../scripts/glue.py");
 
 // Payload to be passed to glue script
 // contains bettertest library and pipedef
+// TODO: I feel like there's a better way to do this than jamming both in stdin
+// Feels like these should be proper files, in /tmp or in-memory or something
 pub fn stdin_payload(pipedef: &str) -> Vec<u8> {
     serde_json::to_vec(&serde_json::json!({
         "bettertest": LIB,
